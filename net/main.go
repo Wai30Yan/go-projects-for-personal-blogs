27e895e4bf22746ae085f7f1961994d25d59ffc8
Wai Yan <mr.waiyanmyint@gmail.com>
2023-02-20 22:23:36 +1100

    go mod init and testing html parsing

---
+++ net/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	// "golang.org/x/net/html"
+)
+
+func main() {
+	resp, err := http.Get("https://wai30yan.github.io/personal-blog/blogs/nmap/")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	b, _ := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	writeFile(b)
+
+
+
+}
+
+func readFile(fileName string) {
+
+}
+
+func writeFile(b []byte) {
+	f, _ := os.Open("response.txt")
+	f.Write(b)
+	f.Close()
+}
